e612ffdad673d3e302884c259d14b0fea8e020e2
Vasa Rolnik <vasa.rolnik@gmail.com>
2024-07-20 07:51:47 +0200

    Added the guess checking system after clicking the option button

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -4,7 +4,8 @@ extends Node
 @export var options_amount: int = 8
 @export var option_btn: PackedScene
 
-@onready var guess_btn_container: GridContainer = $CenterContainer/VBoxContainer/GuessBtnContainer
+@onready var translate_word: Label = %TranslateWord
+@onready var guess_btn_container: GridContainer = %GuessBtnContainer
 
 var vocabulary: Dictionary = DataLoad.item_data
 var option_words: Dictionary
@@ -12,14 +13,11 @@ var current_words: Array
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Signals.guessed.connect(on_guessed)
 	fill_guess_options(options_amount)
 	print_vocabulary(option_words)
 	pick_current_words(option_words)
-	create_guess_buttons(options_amount)
-
-
-#func pick_current_words():
-	#current_words = vocabulary[vocabulary.keys().pick_random()]
+	create_guess_buttons(option_words)
 
 
 func fill_guess_options(number_of_options):
@@ -33,14 +31,22 @@ func fill_guess_options(number_of_options):
 func pick_current_words(options: Dictionary):
 	var keys: Array = options.keys()
 	current_words = options[keys.min()]
+	translate_word.text = current_words[1]
 	print("Current words we wanna guess are: ", current_words)
 
 
-func create_guess_buttons(number_of_guesses: int):
-	for i in number_of_guesses:
+func create_guess_buttons(options: Dictionary):
+	for i in options:
 		var guess_btn = option_btn.instantiate()
-		guess_btn.text = "test"
+		guess_btn.text = options[i][0]
 		guess_btn_container.add_child(guess_btn)
+
+
+func on_guessed(word: String):
+	if word == current_words[0]:
+		print("Correct guess!")
+	else:
+		print("Wrong!")
 
 
 func print_vocabulary(vocab: Dictionary):
